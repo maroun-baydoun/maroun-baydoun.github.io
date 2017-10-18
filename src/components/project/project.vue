695e30a4961bb035c0b239791c174c454ca38910
Maroun Baydoun <maroun.baydoun@gmail.com>
2017-10-19 00:10:55 +0900

    Updated theme

--- src/components/project/project.vue
+++ src/components/project/project.vue
@@ -3,11 +3,11 @@
     <section class="header">
       <h3>{{project.name}}</h3>
       <nav>
-        <a v-bind:href="project.githubUrl" target="_blank" v-bind:title="project.name+ ' on Guithub'">
-          <i class="fa fa-github-alt"></i>
+        <a v-bind:href="project.githubUrl" target="_blank" rel="noreferrer noopener external" v-bind:title="project.name+ ' on Guithub'" v-bind:aria-title="project.name+ ' on Guithub'">
+          <i class="fa fa-github-alt" aria-hidden="true"></i>
         </a>
-        <a v-bind:href="project.url" v-bind:title="project.name+ ' project page'">
-          <i class="fa fa-file-code-o"></i>
+        <a v-bind:href="project.url" v-bind:title="project.name+ ' project page'" v-bind:aria-title="project.name+ ' project page'">
+          <i class="fa fa-file-code-o" aria-hidden="true"></i>
         </a>
       </nav>
     </section>
--- src/components/social-links/social-links.vue
+++ src/components/social-links/social-links.vue
@@ -1,15 +1,15 @@
 <template>
   <nav>
-    <a href="http://www.maroun-baydoun.com" rel="me home">
+    <a href="http://www.maroun-baydoun.com" rel="me home" title="Maroun Baydoun's homepage" aria-title="Maroun Baydoun's homepage">
       <i class="fa fa-home" aria-hidden="true"></i>
     </a>
-    <a href="https://github.com/maroun-baydoun" target="_blank" rel="external me">
+    <a href="https://github.com/maroun-baydoun" target="_blank" rel="external noreferrer noopener me" title="Github" aria-title="Maroun Baydoun's Github account">
       <i class="fa fa-github" aria-hidden="true"></i>
     </a>
-    <a href="https://stackoverflow.com/users/2185759/maroun-baydoun" target="_blank" rel="external me">
+    <a href="https://stackoverflow.com/users/2185759/maroun-baydoun" target="_blank" rel="external noreferrer noopener me" title="Stack Overflow" aria-title="Maroun Baydoun's Stack Overflow account">
       <i class="fa fa-stack-overflow" aria-hidden="true"></i>
     </a>
-    <a href="https://linkedin.com/in/marounbaydoun" target="_blank" rel="external me">
+    <a href="https://linkedin.com/in/marounbaydoun" target="_blank" rel="external noreferrer noopener me" title="Linkedin" aria-title="Maroun Baydoun's Linkedin account">
       <i class="fa fa-linkedin" aria-hidden="true"></i>
     </a>
   </nav>
