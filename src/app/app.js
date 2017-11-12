93867642a8c7c133a609a2b065a138756670e476
Maroun Baydoun <maroun.baydoun@gmail.com>
2017-11-12 22:46:39 +0900

    Updated project url

--- src/app/app.js
+++ src/app/app.js
@@ -15,7 +15,7 @@ export default {
     return {
       projects: [
         new Project('new-hope', 'Esperanto Tokenizer', 'http://www.maroun-baydoun.com/projects/new-hope/', 'https://github.com/maroun-baydoun/new-hope', ['TypeScript', 'Webpack']),
-        new Project('Mediaq', 'Listen to media query updates in JavaScript', 'http://dev.maroun-baydoun.com/mediaq/', 'https://github.com/maroun-baydoun/mediaq', ['TypeScript', 'Webpack']),
+        new Project('Mediaq', 'Listen to media query updates in JavaScript', 'http://www.maroun-baydoun.com/projects/mediaq/', 'https://github.com/maroun-baydoun/mediaq', ['TypeScript', 'Webpack']),
         new Project('Evented', 'Fire and listen to events in JavaScript', 'http://dev.maroun-baydoun.com/evented/', 'https://github.com/maroun-baydoun/evented', ['TypeScript', 'Webpack'])
       ]
     }
