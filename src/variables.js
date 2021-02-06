91a275a36d86a32ab6225d8e508576a4f026d03b
Maroun Baydoun <maroun.baydoun@gmail.com>
2021-02-06 18:30:02 +0100

    Add content

--- src/variables.js
+++ src/variables.js
@@ -1,8 +1,17 @@
 module.exports = {
-  title: "",
-  description: "",
-  heading1: "",
-  heading2: "",
+  title: "Open source projects",
+  description: "Open source projects created by Maroun Baydoun",
+  heading1: "Maroun Baydoun",
+  heading2: "Open source projects",
   githubLink: "",
   npmLink: "",
+
+  projects: [
+    { name: "reaflex", description: "Easy Flexbox in React" },
+    {
+      name: "react-appear-in",
+      description: "Make React components appear sometime in the future",
+    },
+    { name: "new-hope", description: "Esperanto tokenizer" },
+  ],
 };
