67010e625154a00dca9594c2e39ce41e763e401e
Maroun Baydoun <maroun.baydoun@gmail.com>
2021-03-29 11:14:48 +0200

    Update projects

--- src/variables.js
+++ src/variables.js
@@ -15,16 +15,16 @@ module.exports = {
       name: "react-appear-in",
       description: "Make React components appear sometime in the future",
     },
-    {
-      name: "use-media-query",
-      description: "Listen to media query matches in React",
-    },
+    { name: "evented", description: "Fire and listen to events in JavaScript" },
     { name: "new-hope", description: "Esperanto tokenizer" },
     {
       name: "youtube-controller-extension",
       description: "Control Youtube™ videos from any web page",
     },
-    { name: "evented", description: "Fire and listen to events in JavaScript" },
+    {
+      name: "use-media-query",
+      description: "Listen to media query matches in React",
+    },
     {
       name: "de-noun-parser",
       description: "Parse Wikitext's de-noun template",
